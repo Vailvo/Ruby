b4cf686c61127a89843243be1fb863ca003dfef4
Shazé Williams <shazewilliams@live.com>
2021-04-29 02:42:20 -0700

    update on arrays.rb
    methods and conditions

--- ruby_projects/arrays.rb
+++ ruby_projects/arrays.rb
@@ -14,3 +14,19 @@ a.append("Vail")
 a.uniq
 # strips duplicates from arrays
 # ["Vail", 1, 2, 3, 4, 5, 6, 7, 8]
+a.empty?
+# false
+b = []
+b.empty?
+# true
+# You can test the condition of a variable
+a.include?("Vail")
+# true
+a.include?("Arvia")
+# false
+a.push("new item")
+# ["Vail", 1, 2, 3, 4, 5, 6, 7, 8, "new item"]
+# if you you want to remove something from the array, but store it:
+b = a.pop
+# "new item" the pop method removes the last index in the array and the last index was assigned to b
+# a = ["Vail", 1, 2, 3, 4, 5, 6, 7, 8]
